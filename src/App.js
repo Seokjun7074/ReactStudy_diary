9142d2c6c5d017a0b0b8b49bf050ddb7b002119b
seokjun <sukjune96@gmail.com>
2022-03-10 23:22:20 +0900

    컴포넌트간 데이터 전달

--- src/App.js
+++ src/App.js
@@ -1,43 +1,61 @@
+import { useState, useRef } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const dummyData = [
-  {
-    id: 1,
-    author: "jun",
-    content: "Hi~!",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "lala",
-    content: "Hi~!!!!!",
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "potter",
-    content: "Hello~!",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "july",
-    content: "Bon Jour~!",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-];
+// const dummyData = [
+//   {
+//     id: 1,
+//     author: "jun",
+//     content: "Hi~!",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "lala",
+//     content: "Hi~!!!!!",
+//     emotion: 2,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "potter",
+//     content: "Hello~!",
+//     emotion: 4,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     author: "july",
+//     content: "Bon Jour~!",
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div>
-      <DiaryEditor />
-      <DiaryList diatyList={dummyData} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 
-const DiaryEditor = () => {
+const DiaryEditor = ({ onCreate }) => {
   const authorInput = useRef();
   const contentInput = useRef();
 
@@ -24,6 +24,12 @@ const DiaryEditor = () => {
       contentInput.current.focus();
       return;
     }
+    onCreate(state.author, state.content, state.emotion);
+    setState({
+      author: "",
+      content: "",
+      emotion: 1,
+    });
     alert("저장됨");
   };
   return (
